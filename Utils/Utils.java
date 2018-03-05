5097c6df6f6ef498e6d114043369c99cef627f0d
Amenjsajs <amenjsajs@gmail.com>
2018-03-05 10:37:09 +0000

    Methode de récupération des images

--- Utils/Utils.java
+++ Utils/Utils.java
@@ -1,6 +1,13 @@
 package Evenementielle.AJS.Puissance4.Utils;
 
+import Evenementielle.AJS.Puissance4.PS4Init;
+
+import javax.swing.*;
+
 public class Utils {
+    public static ImageIcon getAppIcon(String nomIcon) {
+        return new ImageIcon(PS4Init.class.getResource("/Evenementielle/AJS/Puissance4/Image/"+nomIcon));
+    }
     public static int getRandomIntInclusive(int min, int max) {
         min = (int) Math.ceil(min);
         max = (int) Math.floor(max);
