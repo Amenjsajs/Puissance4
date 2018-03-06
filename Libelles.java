b98f431f3be204e15a19a7d9de4a22ea99b26018
Amenjsajs <amenjsajs@gmail.com>
2018-03-06 08:40:25 +0000

    Classe de chaines de caractères statics

---
+++ Libelles.java
@@ -0,0 +1,13 @@
+package Evenementielle.AJS.Puissance4;
+
+public class Libelles {
+    public static final String LIB_DIALOG_NOM_JOUEUR = "Saisir le nom des joueurs";
+    public static final String LIB_DIALOG_MATCH_NULL = "Match nul ";
+    public static final String LIB_DIALOG_CONTINUER = "\nContinuez le jeu ?";
+    public static final String LIB_DIALOG_RESET = "Voulez-vous vraiment réinitialiser le jeu ?";
+    public static final String LIB_DIALOG_VICTOIRE = "Victoire de ";
+
+    public static final String LIB_BTN_RESET = "Réinitialiser";
+    public static final String LIB_LAB_JOUEUR_COURANT = "Joueur courant ";
+    public static final String LIB_LAB_SCORE = "Score";
+}
